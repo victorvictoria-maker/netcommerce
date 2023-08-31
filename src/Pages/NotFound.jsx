111bcbf5ffd30a2da1e669b418954dbacf517c8e
victorvictoria-maker <victorvictoria0001@gmail.com>
2023-08-31 05:47:57 +0100

    Not found page fixed

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="container text-center py-5">
+    <h1 className="display-4">404</h1>
+    <p className="lead">Oops! Page not found.</p>
+    <p>It looks like the page you're looking for does not exist.</p>
+    <Link to="/" className="btn btn-danger">Go to Homepage</Link>
+  </div>
   )
 }
