28d8aea3eabe9688e6990ba296dbdf9013809efb
victorvictoria-maker <victorvictoria0001@gmail.com>
2023-08-31 06:13:59 +0100

    Domentation page got a heading text

--- src/Pages/Documentation.jsx
+++ src/Pages/Documentation.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export const Documentation = () => {
   return (
     <div className='container'>
+      <h2 className='text-center fw-bold mb-3 mb-lg-4 text-danger'>Project Summary</h2>
       <p>Having acquired proficiency in React, I took the initiative to apply my acquired knowledge, resulting in the creation of Netcommerce. This web application was crafted using ReactJS for the frontend, while Local Storage facilitated data management.</p>
       <p>Incorporating React Query, I efficiently fetched APIs from <a href="https://fakestoreapi.com/products">fakestore api</a> and employed it for data caching. The application's routing was handled through React Router.</p>
       <p>To ensure effective state management of data and functions, I harnessed the power of the useContext hook, while the useState hook also played a pivotal role in the project.</p>
